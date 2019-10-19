524f5ad168c16df7b8c7927a78a8fd75b6328ec9
Denis Toropin <denis.toropin@yandex.ru>
2019-10-19 20:11:09 +0300

    add components skill + slider (correct css)

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,33 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+const diagramm = {
+  template: '#skills-diagramm',
+  props: ['name', 'percent'],
+  methods: {
+    drawPercentCircle() {
+      const elem = this.$refs['percent-circle'];
+      elem.style.strokeDasharray = this.percent + ', 100';
+    }
+  },
+  mounted() {
+    this.drawPercentCircle();
+  }
+}
+
+const chart = {
+  template: '#skills-chart',
+  components: { diagramm },
+  props: ['skill']
+}
+
+new Vue({
+  el: '#skills-component',
+  template: '#skills-container',
+  data: () => ({
+    skills: []
+  }),
+  components: { chart },
+  created() {
+    this.skills = require('../data/skills.json');
+  }
+})
