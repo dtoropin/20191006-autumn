a05742f6e1cfc7fe9f94df40a07964d943ea6760
Denis Toropin <denis.toropin@yandex.ru>
2019-10-20 19:25:58 +0300

    code review 3 + mobile-menu

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
+import './styles/main.pcss';
+if (process.env.NODE_ENV === 'development') {
+  require('file-loader!./index.pug');
 }
 
-import "./scripts/skills";
-import "./scripts/works";
-import "./scripts/comments"
-import "./scripts/parallax";
+import './scripts/skills';
+import './scripts/works';
+import './scripts/comments';
+import './scripts/parallax';
+import './scripts/mobile-menu';
