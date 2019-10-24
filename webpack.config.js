9a951951edc896ef3eccdc81ea3af1eebb7ab767
Denis Toropin <denis.toropin@yandex.ru>
2019-10-24 19:15:29 +0300

    собран dist

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = './'; // /20191006-autumn/dist/
+  const publicPath = '/20191006-autumn/dist/'; // /dist/
 
   const pcss = {
     test: /\.(p|post|)css$/,
